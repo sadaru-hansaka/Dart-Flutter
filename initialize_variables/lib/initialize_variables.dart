91e72581f721652dff2602c789be6d2660687609
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2024-12-17 07:03:55 +0530

    Variables

---
+++ initialize_variables/lib/initialize_variables.dart
@@ -0,0 +1,13 @@
+void display_details(){
+  int age = 19;
+  double height = 5.8;
+  String name = "Sadaru Hansaka";
+  bool isStudent = true;
+
+  print('Name : #$name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is a Student: $isStudent');
+}
+
+
---
+++ initialize_variables/test/initialize_variables_test.dart
@@ -0,0 +1,6 @@
+// import 'package:initialize_variables/initialize_variables.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+  
+// }
